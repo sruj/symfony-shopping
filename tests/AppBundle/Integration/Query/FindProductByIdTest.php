70f2a1069db5c710ee39daafda6308afd97eb7ac
Marcin Kukliński <mkuklinski@getso.pl>
2017-03-24 09:22:17 +0100

    Empty test cases

---
+++ tests/AppBundle/Integration/Query/FindProductByIdTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class FindProductByIdTest extends TestCase
+{
+    public function testFind()
+    {
+        
+    }
+}
