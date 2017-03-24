46b7a1ae4deb6f08589b11108895def1f4030daf
Marcin Kukliński <mkuklinski@getso.pl>
2017-03-24 09:12:24 +0100

    Loading services from AppBundle directory

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -3,7 +3,12 @@
 namespace AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use AppBundle\DependencyInjection\AppBundleExtension;
 
 class AppBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new AppBundleExtension();
+    }
 }
---
+++ src/AppBundle/DependencyInjection/AppBundleExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class AppBundleExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, 
+            new FileLocator([__DIR__.'/../Resources/config'])
+        );
+        $loader->load('services.yml');
+    }
+}
